b05198b735582be2c8341f9b5cb604d183d5ec0f
s-takenoshita <s.takenoshita@gmail.com>
2021-11-13 10:11:20 +0900

    new.html に from を追加。

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # root to: 'home#index'
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new]
+  resources :articles, only: [:show, :new, :create]
 end
