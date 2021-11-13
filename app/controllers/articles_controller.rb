12642349b27762fc9ba264eb6cfb692c19a6e020
s-takenoshita <s.takenoshita@gmail.com>
2021-11-13 14:27:38 +0900

    日本語化しました。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
         if @article.save
             redirect_to article_path(@article), notice: '保存できました'
         else
+            flash.now[:error] = '保存に失敗しました'
             render :new
         end
     end
