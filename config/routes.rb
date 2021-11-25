931ccd43a09e5a4a258b01ce86ec8383f7891848
s-takenoshita <s.takenoshita@gmail.com>
2021-11-25 11:03:20 +0900

    like : create, destroy を実装。

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   root to: 'articles#index'
 
   resources :articles do
-    resource :comments, only: [:new, :create]
+    resources :comments, only: [:new, :create]
+    resource :like, only:[:create, :destroy]
   end
 
   resource :profile, onry: [:show, :edit, :update]
