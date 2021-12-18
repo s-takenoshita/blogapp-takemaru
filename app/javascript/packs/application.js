0f1245e56c2f35535f08825c9c256dbde703546c
s-takenoshita <s.takenoshita@gmail.com>
2021-12-18 14:09:29 +0900

    jQueryの動作確認。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,10 @@ require("channels")
 require("trix")
 require("@rails/actiontext")
 
+import $ from 'jquery'
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('.article_title').on('click', () => {
+    window.alert('Clicked')
+  })
+})
