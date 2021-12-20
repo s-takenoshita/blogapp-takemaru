99a0e466fe002445581147c742fa01e5b02496e6
s-takenoshita <s.takenoshita@gmail.com>
2021-12-20 22:15:26 +0900

    コメントの入力を実装。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,12 +14,8 @@ class CommentsController < ApplicationController
     def create
         article = Article.find(params[:article_id])
         @comment = article.comments.build(comments_params)
-        if @comment.save
-            redirect_to article_path(article), notice: 'コメントを追加しました。'
-        else
-            flash.now[:error] = '保存に失敗しました。'
-            render :new
-        end
+        @comment.save!
+        render json: @comment
     end
 
     private
