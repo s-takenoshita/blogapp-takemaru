34c36636be31d3b697b5b2f2badf6d187fca77c7
s-takenoshita <s.takenoshita@gmail.com>
2021-12-27 21:40:32 +0900

    contextを使用。

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'タイトルと記事が入力されていれば記事を保存できる。' do
-    user = User.create!({
-      email: 'test@example.com',
-      password: '99999999'
-    })
-    article = user.articles.build({
-      title: Faker::Lorem.characters(number: 20),
-      content: Faker::Lorem.characters(number: 100)
-    })
-    expect(article).to be_valid
+  context 'タイトルと記事が入力されている場合' do
+    before do
+      user = User.create!({
+        email: 'test@example.com',
+        password: '99999999'
+      })
+      @article = user.articles.build({
+        title: Faker::Lorem.characters(number: 20),
+        content: Faker::Lorem.characters(number: 100)
+      })
+    end
+    it '記事を保存できる。' do
+      expect(@article).to be_valid
+    end
   end
 end
