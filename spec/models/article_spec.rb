6a5e7a7504f38b7da19bebb2d1c3b89c908ba520
s-takenoshita <s.takenoshita@gmail.com>
2021-12-27 21:33:21 +0900

    最初のテストを作成し実行。

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'タイトルと記事が入力されていれば記事を保存できる。' do
+    user = User.create!({
+      email: 'test@example.com',
+      password: '99999999'
+    })
+    article = user.articles.build({
+      title: Faker::Lorem.characters(number: 20),
+      content: Faker::Lorem.characters(number: 100)
+    })
+    expect(article).to be_valid
+  end
 end
