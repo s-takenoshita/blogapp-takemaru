207ab47fd399ca3e6be2875f489fdcce4b4e591a
s-takenoshita <s.takenoshita@gmail.com>
2021-12-30 11:13:56 +0900

    userを作る時に同時にprofileを作る。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,4 +3,10 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { '99999999' }
   end
+
+  trait :with_profile do
+    after :build do |user|
+      build(:profile, user: user)
+    end
+  end
 end
